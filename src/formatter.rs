6bae426ac2b6e95cbd14ee1f77243e2d6e3b23f7
Donnerinoern <72634505+Donnerinoern@users.noreply.github.com>
2024-04-10 17:07:55 +0200

    formatter: url-formatting for markdown

--- src/formatter.rs
+++ src/formatter.rs
@@ -6,16 +6,45 @@ pub fn format(map: &mut HashMap<FieldType, String>, file_extension: &FileExtensi
         let subject = format_subject(&mut map.get(&FieldType::Subject).unwrap().to_string());
         map.remove(&FieldType::Subject);
         map.insert(FieldType::Subject, subject);
-        let content = format_text(&mut map.get(&FieldType::ContentPlain).unwrap().to_string());
+        let content = format_text(&mut map.get(&FieldType::ContentPlain).unwrap().to_string(), file_extension);
         map.remove(&FieldType::ContentPlain);
         map.insert(FieldType::ContentPlain, content);
     }
 }
 
-fn format_text(text_string: &mut String) -> String {
+fn format_text(text_string: &mut String, file_extension: &FileExtension) -> String {
     *text_string = text_string.replace("=E5", "å");
     *text_string = text_string.replace("=F8", "ø");
-    text_string.to_string()
+    match file_extension {
+        FileExtension::MD => {
+            let mut new_string = String::new();
+            for word in text_string.split(" ") {
+                if word.contains("<http") && !word.contains("[") {
+                    let mut s = String::from("[".to_owned() + word);
+                    s = s.replace("<", "](");
+                    s = s.replace(">", ")");
+
+                    new_string.push_str(s.as_str());
+                    new_string.push(' ');
+                } else if word.contains("<http") && word.contains("[") {
+                    let mut s = String::from(word);
+                    s = s.replace("<", "(");
+                    s = s.replace(">", ")\n");
+
+                    new_string.push_str(s.as_str());
+                    new_string.push(' ');
+                } else {
+                    new_string.push_str(word);
+                    new_string.push(' ');
+                }
+            }
+            new_string
+        }
+
+        _ => {
+            text_string.to_string()
+        }
+    }
 }
 
 fn format_subject(string: &mut String) -> String {
